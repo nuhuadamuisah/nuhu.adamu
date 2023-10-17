5d2e96f481ba8a36dc01c79f7116174d8e6cd992
nuhuadamuisah <147081701+nuhuadamuisah@users.noreply.github.com>
2023-10-17 22:22:34 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,25 @@
+/*=================Preloder animation=================*/
+const loader = document.querySelector('.preloader')
+
+setTimeout(() => {
+  loader.style.display ="none";
+}, 6600);
+
+/*=================typying animation=================*/
+var typed = new Typed(".typing", {
+    strings: ["Web Designer", "React Developer", "Data Analyst", "Cybersecurity Enthusiast"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    backDelay: 1000,
+    startDelay: 1000,
+    loop: true,
+ });
+
+ var typed = new Typed(".typing-2", {
+    strings: ["Web Designer", "React Developer", "Data Analyst", "Cybersecurity Enthusiast"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    backDelay: 1000,
+    startDelay: 1000,
+    loop: true,
+ });
